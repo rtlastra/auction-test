849547120c5713a258ed0ac1ae94dec47de2bf19
Rafael Lastra <rtlastra@uninorte.edu.co>
2021-04-10 15:54:57 -0500

    Add versioning to the application

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json }, path: "/" do
+    namespace :v1 do
+      resources :users
+      resources :sessions, only: %i[create]
+      resources :biddings
+    end
+  end
 end
