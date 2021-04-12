eb5a7207f4611f4750e63fc76b0a54022557c007
Rafael Lastra <rtlastra@uninorte.edu.co>
2021-04-12 00:59:57 -0500

    Add authorize in biddings endpoints

--- server/app/controllers/api/v1/biddings_controller.rb
+++ server/app/controllers/api/v1/biddings_controller.rb
@@ -3,52 +3,51 @@ class Api::V1::BiddingsController < Api::V1::ApplicationController
   before_action :authorize_request
 
   def index
-    data = {
-      biddings: Bidding.where(status: registered)
-    }
-    render_json_response(data)
+    authorize Bidding
+    biddings = Bidding.where(status: :registered)
+    render_json_response(biddings)
   end
 
   def show
+    authorize Bidding
     records = HistoricUserBidding.where(id: @bidding.id)
     data = {
       biddings: @bidding,
-      article: @bidding.article,
       users_history: records
     }
     render_json_response(data)
   end
 
   def create
+    authorize Bidding
     @bidding = Bidding.new(create_bidding_params)
     if @bidding.save
-      data = {
-        message: 'Subasta creada exitosamente'
-      }
-      render_json_response(data)
+      render_json_response(@bidding)
     else
       render_json_api_general_error(@bidding.errors.full_messages, :unprocessable_entity)
     end
   end
 
   def update
-    if @bidding.update(update_bidding_params)
-      HistoricUserBidding.create(user_id: update_bidding_params[:user_id], bidding_id: update_bidding_params[:bidding_id], status: update_bidding_params[:status])
-      data = {
-        message: 'Subasta actualizada exitosamente'
-      }
-      render_json_response(data)
+    authorize Bidding
+    if (update_bidding_params[:actual_price].to_i > @bidding.actual_price.to_i && update_bidding_params[:actual_price].to_i > @bidding.starting_price.to_i)
+      if @bidding.update(update_bidding_params)
+        HistoricUserBidding.create!(user_id: update_bidding_params[:user_id], bidding_id: @bidding.id, status: @bidding.status)
+        message = 'Oferta realizada actualizada exitosamente'
+        render_json_response(message)
+      else
+        render_json_api_general_error(@bidding.errors.full_messages, :unprocessable_entity)
+      end
     else
-      render_json_api_general_error(@bidding.errors.full_messages, :unprocessable_entity)
+      render_json_api_general_error('El valor a ofertar tiene que ser mayor al actual', :unprocessable_entity)
     end
   end
 
   def destroy
+    authorize Bidding
     @bidding.destroy
-    data = {
-      message: 'Subasta eliminada exitosamente'
-    }
-    render_json_response(data)
+    message = 'Subasta eliminada exitosamente'
+    render_json_response(message)
   end
 
   private
@@ -57,7 +56,7 @@ class Api::V1::BiddingsController < Api::V1::ApplicationController
     end
 
     def create_bidding_params
-      params.permit(:initial_date, :final_date, :starting_price, :article_id, :status)
+      params.permit(:initial_date, :final_date, :starting_price, :status, :name, :description)
     end
 
     def update_bidding_params
