8340e885ca9e69291699400d8698b85eb2b90f18
Rafael Lastra <rtlastra@uninorte.edu.co>
2021-04-12 01:03:56 -0500

    Create has_permissions method in user model

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   #Validations
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true
   validates :password,
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
@@ -14,5 +14,26 @@ class User < ApplicationRecord
   has_many :historic_user_biddings
   has_many :schedulings
   has_many :permissions, through: :role, source: :permissions
+
+  after_initialize :init
+
+    def init
+      self.role_id  ||= Role.find_by_name('seller').id
+      self.username ||= generate_username(self.name+' '+self.last_name) 
+    end
+
+    def generate_username(fullname)
+      ActiveSupport::Inflector.transliterate(fullname) 
+        .downcase
+        .strip      
+        .gsub(/[^a-z]/, '_')
+        .gsub(/\A_+/, '')    
+        .gsub(/_+\Z/, '') 
+        .gsub(/_+/, '_')    
+    end
+
+    def has_permission?(action)
+      permission = Permission.find_by(action: action)
+      self.permissions.include?(permission)
+    end
 end
- 
