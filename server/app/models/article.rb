32801162026c143903cdb624ad16385c4d923b34
Rafael Lastra <rtlastra@uninorte.edu.co>
2021-04-12 01:01:33 -0500

    Delete article model

--- server/app/models/article.rb
+++
@@ -1,8 +0,0 @@
-class Article < ApplicationRecord
-  #Validations
-  validates :name, presence: true
-  validates :description, presence: true
-
-  #Associations
-  belongs_to :bidding, required: false
-end
