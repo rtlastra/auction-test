a89605540cbb043bbc82a3ca02aa9d819f7ef158
Rafael Lastra <rtlastra@uninorte.edu.co>
2021-04-12 01:03:32 -0500

    Add permissions to seller role

--- server/app/seed_builders/role_seed_builder.rb
+++ server/app/seed_builders/role_seed_builder.rb
@@ -4,5 +4,10 @@ class RoleSeedBuilder < ApplicationSeedBuilder
     Role.create(name: 'seller') unless Role.find_by_name('seller').present?
     # Add permissions to roles
     Role.find_by_name(:admin).permissions = Permission.all
+    Role.find_by_name(:seller).permissions << Permission.find_by_action('sign_in')
+    Role.find_by_name(:seller).permissions << Permission.find_by_action('can_update_bidding')
+    Role.find_by_name(:seller).permissions << Permission.find_by_action('can_list_biddings')
+    Role.find_by_name(:seller).permissions << Permission.find_by_action('can_show_bidding')
+    Role.find_by_name(:seller).permissions << Permission.find_by_action('can_show_user')
   end
 end
